ae44ce6e7aa556900ecb5f1ea44a48c64adcc987
Richard Lam <richardvclam@gmail.com>
2020-07-14 12:33:50 -0700

    feat: fade dividers on active segment

--- src/SegmentedControl/SegmentedControlStyles.ts
+++ src/SegmentedControl/SegmentedControlStyles.ts
@@ -28,17 +28,6 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.5,
     shadowRadius: 2,
   },
-  dividerContainer: {
-    paddingTop: 7,
-    paddingBottom: 7,
-    zIndex: 0,
-  },
-  divider: {
-    height: '100%',
-    width: 1,
-    borderWidth: 0,
-    backgroundColor: 'rgba(120, 120, 120, 0.2)',
-  },
 });
 
 export default styles;
